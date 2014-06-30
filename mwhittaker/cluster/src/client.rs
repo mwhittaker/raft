05914e0416a70a9125469f2286c1992c34e1b50f
Michael Whittaker <mjw297@cornell.edu>
2014-06-30 03:39:31 +0000

    Removed compiler warnings

--- mwhittaker/cluster/src/client.rs
+++ mwhittaker/cluster/src/client.rs
@@ -4,7 +4,7 @@ pub static ADDRESS : &'static str = "127.0.0.1";
 pub static PORT    : u16          = 8080;
 
 fn main() {
-    TcpStream::connect(ADDRESS, PORT)
+    let _ = TcpStream::connect(ADDRESS, PORT)
     .map(|mut stream| {
         stream.read_to_str().map(|msg| {println!("{}", msg)})
     });
--- mwhittaker/cluster/src/server.rs
+++ mwhittaker/cluster/src/server.rs
@@ -5,7 +5,7 @@ pub static ADDRESS : &'static str = "127.0.0.1";
 pub static PORT    : u16          = 8080;
 
 fn handle_stream(mut stream: TcpStream) {
-    stream.write_str("hello");
+    let _ = stream.write_str("hello");
 }
 
 fn main() {
@@ -13,7 +13,7 @@ fn main() {
     .map(|listener| { listener.listen() })
     .map(|mut acceptor| {
         for stream in acceptor.incoming() {
-            stream.map(|stream| { 
+            let _ = stream.map(|stream| { 
                 spawn( proc(){handle_stream(stream)} );
             });
         }
