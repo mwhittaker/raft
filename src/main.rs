a72c3971725d9e1213ed4ef75f87cbce6bda1aa6
Michael Whittaker <mjw297@cornell.edu>
2014-08-12 04:39:44 +0000

    Begun work on many to many network.

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,62 @@
 use std::os;
+use std::io::{TcpListener, TcpStream, Listener, Acceptor};
 
-pub mod server;
-pub mod client;
+type Address = (String, u16);
 
-static SERVER_HOST: &'static str = "127.0.0.1";
-static SERVER_PORT: u16          = 9999;
-
-fn usage() {
-    println!("raft (client|server)");
+fn usage() -> &'static str {
+    "raft bind_address [connect addresses]..."
 }
 
 fn main() {
-    let args = os::args();
+    let mut args = os::args().move_iter();
+    
+    let _ /* executable name */ = args.next();
+    let bind_address = args.next().expect(usage());    
+    let connect_addresses = args.collect();
+    listen_and_connect(bind_address, connect_addresses);
+}
 
-    if args.len() < 2 {
-        usage();
-        os::set_exit_status(1);
-        return;
+fn listen_and_connect(bind_address: String, connect_addresses: Vec<String>) {
+    let bind_address = parse_address(bind_address).expect("couldn't parse bind address");
+    let connect_addresses = connect_addresses.move_iter().map(|s| {
+        parse_address(s).expect("couldn't parse listen address")
+    }).collect();
+    lac(bind_address, connect_addresses);
+}
+
+fn lac(bind_address: Address, connect_addresses: Vec<Address>) {
+    //spawn(proc() {serve(bind_address)});
+    //spawn(proc() {connect(connect_addresses)});
+}
+
+fn parse_address(address: String) -> Option<Address> {
+    let hostport: Vec<&str> = address.as_slice().split(':').collect();
+    if hostport.len() < 2 {
+        None
+    } else {
+        let host = hostport[0];
+        let port: u16 = match from_str(hostport[1]) {
+            Some(port) => port,
+            None => {
+                println!("could not parse port");
+                return None;
+            }
+        };
+        Some((String::from_str(host), port))
+    }
+}
+
+#[test]
+fn foo() {
+    fn assert(expected: Option<Address>, actual: &str) {
+        let actual = parse_address(String::from_str(actual));
+        assert!(expected == actual);
     }
 
-    match args[1].as_slice() {
-        "server" => server::main(),
-        "client" => client::main(),
-        _        => usage(),
-    };
+    assert(None, "");
+    assert(None, "localhost");
+    assert(None, "8080");
+    assert(None, "localhost8080");
+    assert(None, "localhost:");
+    assert(Some((String::from_str("localhost"), 8080u16)), "localhost:8080");
 }
