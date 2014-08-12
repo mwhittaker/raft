519a3e1742ab1ea7d584783915f4e717d99aca08
Michael Whittaker <mjw297@cornell.edu>
2014-08-12 06:25:42 +0000

    Finished network!

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use std::os;
 use help::{usage, parse_address};
 
+mod server;
+mod client;
+
+static CLIENT_PING_PERIOD: u64 = 2000;
+
 type Address = (String, u16);
 
 <<<<<<< HEAD
@@ -21,7 +26,7 @@ fn usage() -> &'static str {
 >>>>>>> Tidied things up.
 fn main() {
     let mut args = os::args().move_iter();
-    
+
     let _          = args.next();
     let bind_addr  = args.next().expect(usage());
     let conn_addrs = args.collect();
@@ -33,15 +38,24 @@ fn network(bind_addr: String, conn_addrs: Vec<String>) {
     let conn_addrs = conn_addrs.move_iter().map(|s| {
         parse_address(s).expect(usage())
     }).collect();
-    
+
     _network(bind_addr, conn_addrs);
 }
 
 fn _network(bind_addr: Address, conn_addrs: Vec<Address>) {
-    let _ = bind_addr;
-    let _ = conn_addrs;
-    //spawn(proc() {serve(bind_addr)});
-    //spawn(proc() {connect(conn_addrs)});
+    let (host, port) = bind_addr;
+    let msg = format!("hello from {}:{}\n", host, port);
+
+    spawn(proc() {
+        server::serve(host.as_slice(), port);
+    });
+
+    for (host, port) in conn_addrs.move_iter() {
+        let msg = msg.clone();
+        spawn(proc() {
+            client::connect(host.as_slice(), port, msg.as_slice());
+        });
+    }
 }
 
 mod help {
@@ -51,10 +65,10 @@ mod help {
 
     pub fn parse_address(address: String) -> Option<super::Address> {
         let hostport: Vec<&str> = address.as_slice().split(':').collect();
-        
+
         if hostport.len() < 2 {
             return None
-        } 
+        }
 
         match (String::from_str(hostport[0]), from_str(hostport[1])) {
             (host, Some(port)) => Some((host, port)),
