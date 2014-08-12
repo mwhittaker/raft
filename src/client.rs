d7f3e2e78e48befb380c9b26a97560e4f224de32
Michael Whittaker <mjw297@cornell.edu>
2014-08-12 06:25:42 +0000

    Finished network!

--- src/client.rs
+++ src/client.rs
@@ -1,8 +1,23 @@
-use super::{SERVER_HOST, SERVER_PORT, CLIENT_PING_PERIOD};
+use super::{CLIENT_PING_PERIOD};
+use std::io::timer;
 use std::io::{TcpStream, Timer};
 
-pub fn main() {
-    let mut stream = TcpStream::connect(SERVER_HOST, SERVER_PORT);
+fn get_stream(host: &str, port: u16) -> TcpStream {
+    println!("connecting to {}:{}", host, port);
+    loop {
+        match TcpStream::connect(host, port) {
+            Ok(stream) => {
+                return stream;
+            },
+            Err(_) => {
+                timer::sleep(1000);
+            }
+        }
+    }
+}
+
+pub fn connect(host: &str, port: u16, msg: &str) {
+    let mut stream = get_stream(host, port);
 
     let mut timer = {
         match Timer::new() {
@@ -16,8 +31,8 @@ pub fn main() {
     let periodic = timer.periodic(CLIENT_PING_PERIOD);
 
     loop {
-        periodic.recv(); 
-        match stream.write_str("hello\n") {
+        periodic.recv();
+        match stream.write_str(msg) {
             Ok(()) => {},
             Err(e) => {println!("{}", e);},
         }
