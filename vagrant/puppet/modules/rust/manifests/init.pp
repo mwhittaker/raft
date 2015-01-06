30310acef58bcd90ad13a568140108d2d37f2b70
Michael Whittaker <mjw297@cornell.edu>
2015-01-06 14:19:18 -0500

    Made some VM changes.

--- vagrant/puppet/modules/rust/manifests/init.pp
+++ vagrant/puppet/modules/rust/manifests/init.pp
@@ -1,18 +1,18 @@
 include git
 
 class rust {
-    exec { "wget-rust": 
-        command => "wget http://rust-lang.org/rustup.sh",
+    exec { "wget-rust":
+        command => "wget https://static.rust-lang.org/rustup.sh",
         creates => "/home/vagrant/rustup.sh",
         cwd     => "/home/vagrant",
     }
-     
+
     exec { "chmod-rust":
         command => "chmod 755 rustup.sh",
         cwd     => "/home/vagrant",
         require => Exec["wget-rust"],
     }
-    
+
     exec { "install-rust":
         command  => "/home/vagrant/rustup.sh",
         creates  => "/usr/local/bin/rustc",
